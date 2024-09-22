5bdccf24c244313524c9198d374b636bd334b4f4
Samuel Juarez <sjuar845@gmail.com>
2024-09-22 02:44:02 -0500

    completed Transaction List component

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -13,7 +13,7 @@
             amount:parseFloat(amount.value),
         }
 
-        emit('transationSubmitted', transactionData)
+        emit('transactionSubmitted', transactionData)
 
         text.value = ''
         amount.value = ''
